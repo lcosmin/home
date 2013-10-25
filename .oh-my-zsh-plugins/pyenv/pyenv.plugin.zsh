cf380329dfc416eecacfebf9aef56a7e9750ebde
Cosmin Luță <q4break@gmail.com>
2013-10-25 12:31:45 +0300

    Updated pyenv plugin

--- .oh-my-zsh-plugins/pyenv/pyenv.plugin.zsh
+++ .oh-my-zsh-plugins/pyenv/pyenv.plugin.zsh
@@ -1,7 +1,19 @@
 
-if [[ -x "$(which pyenv)" ]]
+echo -n "[+] Loading pyenv..."
+
+if [[ -d "$HOME/.pyenv" ]] 
 then
-    echo "[+] Loading pyenv..."
-    eval "$(pyenv init -)"
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+
+    if [[ -x "$(which pyenv)" ]]
+    then
+        eval "$(pyenv init -)"
+    fi
+
+    echo "located at $PYENV_ROOT"
+else
+    echo "not found"
 fi
+
 
