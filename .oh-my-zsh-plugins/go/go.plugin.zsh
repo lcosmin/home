12e644eafe3bee74a29630065f7a0683d1248491
Cosmin Luță <q4break@gmail.com>
2015-02-04 19:57:50 +0200

    Updated GO plugin

--- .oh-my-zsh-plugins/go/go.plugin.zsh
+++ .oh-my-zsh-plugins/go/go.plugin.zsh
@@ -1,6 +1,17 @@
 
 echo -n "[+] Setting GO variables..."
-export GOROOT=/usr/local/go
-export PATH=$PATH:$GOROOT/bin
+
+case "$(uname)" in
+    Darwin)
+        export GOROOT=/usr/local/Cellar/go/1.4.1/libexec
+        ;;
+    Linux)
+        export GOROOT=/usr/local/go
+        ;;
+esac
+
+export GOPATH=$HOME/Work/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
 echo "done"
 
