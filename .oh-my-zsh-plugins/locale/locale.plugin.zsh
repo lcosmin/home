dc4b0c6ecc71cec1606a8e65f1cbc584daf74d9f
Cosmin Luță <q4break@gmail.com>
2015-07-22 12:41:30 +0300

    Cleaned up external .zsh repositories

--- .oh-my-zsh-plugins/locale/locale.plugin.zsh
+++
@@ -1,5 +0,0 @@
-
-echo -n "[+] Setting LANG..."
-export LANG="en_US.UTF-8"
-echo "done"
-
--- .oh-my-zsh-plugins/machine-tag/machine-tag.plugin.zsh
+++
@@ -1,9 +0,0 @@
-machine_tag() {
-    _tagfile=~/.machine_tag
-
-    if [[ -f "$_tagfile" ]]; then
-        cat $_tagfile
-    else
-        echo "unknown"
-    fi
-}
