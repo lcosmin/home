212a118f30ea323782d27a5ec384a19b7cf6d9d5
Cosmin Luță <q4break@gmail.com>
2019-11-18 12:18:05 +0200

    Unfreeze git module

--- packages/develop/90_devel.zsh
+++ packages/develop/90_devel.zsh
@@ -1,5 +1,5 @@
-zplug "modules/git", from:prezto, frozen:1
-zplug "modules/python", from:prezto, frozen:1
+zplug "modules/git", from:prezto, frozen:0
+zplug "modules/python", from:prezto, frozen:0
 
 # Allow git to display emojis in the log
 export LESS='--raw-control-chars'
