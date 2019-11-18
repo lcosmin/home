cd795f40e111fcb38077cf40f783bfed065379d7
Cosmin Luță <q4break@gmail.com>
2019-11-18 12:17:36 +0200

    Add new work alias

--- packages/mtag-work/90_aliases_work.zsh
+++ packages/mtag-work/90_aliases_work.zsh
@@ -5,7 +5,9 @@
 alias apc="cloud-connect -p apcc"
 alias auc="cloud-connect -p aucc"
 alias aucw="cloud-connect -p aucw"
+alias aucs="cloud-connect -p aucs"
 alias tl="cloud-connect -p tl"
+alias da="cloud-connect -p da"
 
 
 alias clog-dev="conflog -p apc-dev -a"
@@ -13,4 +15,3 @@ alias clog-acc="conflog -p apc-acc -a"
 alias clog-prod="conflog -p apc-prod -a"
 
 alias xenial="cd ~/Work/deb-16 ; vagrant up ; vagrant ssh"
-alias trusty="cd ~/Work/deb-trusty ; vagrant up ; vagrant ssh"
